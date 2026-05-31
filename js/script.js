3d528298de22af7e4b2c50d89c9452f6a29fd613
Shivam Chauhan <shivamchauhan0215@gmail.com>
2026-05-31 12:43:43 +0530

    feat=add contact form validation

--- js/script.js
+++ js/script.js
@@ -4,3 +4,41 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const form = document.querySelector("#contact-form");
+
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const message = messageInput.value.trim();
+
+    if (
+        nameInput.value === "" ||
+        emailInput.value === "" ||
+        messageInput.value === ""
+    ) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    const emailPattern =
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(emailInput.value)) {
+
+        alert("Please enter a valid email");
+
+        return;
+    }
+    alert("Form submitted successfully!");
+
+    form.reset();
+});
+
